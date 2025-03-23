5d38bccb1cff561a5f35bae964de3916deeb2df4
Phil Choi <20016424+pchae@users.noreply.github.com>
2025-03-23 15:26:46 -0400

    refactor: remove next-themes dependency and simplify Providers component

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,12 +1,7 @@
 'use client'
 
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+import { type PropsWithChildren } from "react"
 
-export function Providers({ children, ...props }: ThemeProviderProps) {
-  return (
-    <NextThemesProvider {...props}>
-      {children}
-    </NextThemesProvider>
-  )
+export function Providers({ children }: PropsWithChildren) {
+  return children
 } 
